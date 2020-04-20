9976d72a4744da67282a6006b0b72bf9f759a06b
Bryce Speirs <baspeirs@gmail.com>
2020-04-19 22:46:56 -0400

    created html, css, and JS files and created the frame of the application

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// first, create the body, we will need 24 sections to fill out a full day
+    // create each section individually in html
+    // assign each section its own id, but the same class
+    // assign each section its corrisponding time (12am to 11:59pm)
+    // give each hour some text area for user input, and a submit button to apply it to the page
+    // use normal time for the visible sections
+    // maybe add a scroll to each input section? 
+
+// wright a script that
+    // is able to grab the users local time(from the browser probably?)
+    // changes the id of each box as hours pass
+        // if time is > section time ---> change class to .grey
+        // if time === section time (roughly) ---> change class to .red
+        // if time < section time ---> class = .green (standard)
+    // use military time for the script
+    // use local time (probalby from browser) to grab the date and append it to the bottom of the header
+
